ec97371b0f949eb43a96ecbf2c436c43e5c95274
Lucas_Gabriel <lugaautomation@gmail.com>
2026-04-03 13:02:13 -0300

    up 08

---
+++ script.js
@@ -0,0 +1,31 @@
+document.querySelector('.copy-email').addEventListener('click', () => {
+    navigator.clipboard.writeText('lucasgsgoncalves@gmail.com');
+
+    const msg = document.createElement('div');
+    msg.innerText = 'Email copiado!';
+    msg.style.position = 'fixed';
+    msg.style.bottom = '20px';
+    msg.style.left = '50%';
+    msg.style.transform = 'translateX(-50%)';
+    msg.style.background = '#5fa08249';
+    msg.style.color = '#fff';
+    msg.style.padding = '10px 20px';
+    msg.style.borderRadius = '8px';
+    msg.style.zIndex = '9999';
+
+    document.body.appendChild(msg);
+
+    setTimeout(() => {
+        msg.remove();
+    }, 2000);
+});
+
+document.getElementById('btnSistema').onclick = () => {
+    window.location.href =
+        'https://drive.google.com/uc?export=download&id=11PN7nsxD3n29l7jlh-adRFpy699KRSdM';
+};
+
+document.getElementById('btnApp').onclick = () => {
+    window.location.href =
+        'https://drive.google.com/uc?export=download&id=1nFWyBmL-4dh8JANbrpp2oEwyuB-Ny2T4';
+};
